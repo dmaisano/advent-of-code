232f04342233500b46012ea7939147c627c50816
Dom <dmaisanooo@gmail.com>
2023-12-29 16:17:13 -0500

    Added day 1 solution

---
+++ src/2023/day-1/index.ts
@@ -0,0 +1,66 @@
+import { readFile } from "fs/promises"
+import { join as joinPath } from "path"
+
+const digitMap: {
+  [key: string]: string
+} = {
+  zero: "0",
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+}
+const digitMapEntries = Object.entries(digitMap)
+
+function isDigit(char: string): boolean {
+  return /^\d$/.test(char)
+}
+
+function getCalibrationValue(line: string): number {
+  const digits: string[] = []
+
+  for (let i = 0; i < line.length; i++) {
+    const char = line[i] ?? ""
+
+    if (isDigit(char)) {
+      digits.push(char)
+      continue
+    }
+
+    const subStr = line.substring(i)
+    for (const [key, value] of digitMapEntries) {
+      if (subStr.startsWith(key)) {
+        digits.push(value)
+        break
+      }
+    }
+  }
+
+  const firstDigit = digits[0] ?? ""
+  const lastDigit = digits[digits.length - 1] ?? ""
+  return parseInt(firstDigit + lastDigit)
+}
+
+async function main() {
+  const lines: string[] = (
+    await readFile(joinPath(__dirname, "./input.txt"), "utf8")
+  )
+    .split("\n")
+    .map((l) => l.trim())
+    .filter((l) => l.length > 0)
+
+  let sum = 0
+  for (const line of lines) {
+    sum += getCalibrationValue(line)
+  }
+
+  console.log(`The sum of all of the calibration values is: ${sum}`)
+  return sum
+}
+
+main().catch(console.error)
