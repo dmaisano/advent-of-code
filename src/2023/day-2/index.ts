5619d87a8da61c6e9fb660ed19e24f282e111259
Dom <dmaisanooo@gmail.com>
2023-12-30 00:44:13 -0500

    Day 2 part 1

---
+++ src/2023/day-2/index.ts
@@ -0,0 +1,65 @@
+import { importFileStrArray } from "../utils"
+
+type Game = {
+  id: number
+  counts: { red: number; green: number; blue: number }[]
+}
+
+function parseGame(s: string): Game {
+  const [idPart, ...countParts] = s.split(": ")
+  const id = parseInt(idPart.replace("Game ", ""), 10)
+  const counts = countParts
+    .join(": ")
+    .split("; ")
+    .map((part) => {
+      const colors = { red: 0, green: 0, blue: 0 }
+      part.split(", ").forEach((colorPart) => {
+        const [count, color] = colorPart.split(" ")
+        colors[color as "red" | "green" | "blue"] = parseInt(count, 10)
+      })
+      return colors
+    })
+  return { id, counts }
+}
+
+function isGamePossible(
+  game: Game,
+  maxCounts: { red: number; green: number; blue: number }
+): boolean {
+  return game.counts.every(
+    (counts) =>
+      counts.red <= maxCounts.red &&
+      counts.green <= maxCounts.green &&
+      counts.blue <= maxCounts.blue
+  )
+}
+
+function sumPossibleGameIds(
+  games: string[],
+  maxCounts: { red: number; green: number; blue: number }
+): number {
+  return games
+    .map(parseGame)
+    .filter((game) => isGamePossible(game, maxCounts))
+    .reduce((sum, game) => sum + game.id, 0)
+}
+
+async function main({ debug }: { debug: boolean } = { debug: false }) {
+  const games: string[] = await importFileStrArray(
+    "./input.txt",
+    debug && [
+      "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    ]
+  )
+
+  const maxCounts = { red: 12, green: 13, blue: 14 }
+  const solution = sumPossibleGameIds(games, maxCounts)
+  console.log(`Sum of IDs of possible games: ${solution}`)
+  return solution
+}
+
+main({ debug: false }).catch(console.error)
--- src/2023/utils/index.ts
+++ src/2023/utils/index.ts
@@ -2,8 +2,11 @@ import { readFile } from "fs/promises"
 import { join as joinPath } from "path"
 
 export const importFileStrArray = async (
-  fileName: string
+  fileName: string,
+  stub: string[] = []
 ): Promise<string[]> => {
+  if (stub?.length > 0) return stub
+
   return (await readFile(joinPath(process.cwd(), fileName), "utf8"))
     .split("\n")
     .map((l) => l.trim())
