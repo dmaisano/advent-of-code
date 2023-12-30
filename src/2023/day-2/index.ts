6956ba48654684f511c19b042cb8e137f9fc923f
Dom <dmaisanooo@gmail.com>
2023-12-30 00:53:28 -0500

    Day 2 Part 2

--- src/2023/day-2/index.ts
+++ src/2023/day-2/index.ts
@@ -22,26 +22,53 @@ function parseGame(s: string): Game {
   return { id, counts }
 }
 
-function isGamePossible(
-  game: Game,
-  maxCounts: { red: number; green: number; blue: number }
-): boolean {
-  return game.counts.every(
-    (counts) =>
-      counts.red <= maxCounts.red &&
-      counts.green <= maxCounts.green &&
-      counts.blue <= maxCounts.blue
+// function isGamePossible(
+//   game: Game,
+//   maxCounts: { red: number; green: number; blue: number }
+// ): boolean {
+//   return game.counts.every(
+//     (counts) =>
+//       counts.red <= maxCounts.red &&
+//       counts.green <= maxCounts.green &&
+//       counts.blue <= maxCounts.blue
+//   )
+// }
+
+// function sumPossibleGameIds(
+//   games: string[],
+//   maxCounts: { red: number; green: number; blue: number }
+// ): number {
+//   return games
+//     .map(parseGame)
+//     .filter((game) => isGamePossible(game, maxCounts))
+//     .reduce((sum, game) => sum + game.id, 0)
+// }
+
+function minCubesForGame(game: Game): {
+  red: number
+  green: number
+  blue: number
+} {
+  return game.counts.reduce(
+    (minCounts, counts) => ({
+      red: Math.max(minCounts.red, counts.red),
+      green: Math.max(minCounts.green, counts.green),
+      blue: Math.max(minCounts.blue, counts.blue),
+    }),
+    { red: 0, green: 0, blue: 0 }
   )
 }
 
-function sumPossibleGameIds(
-  games: string[],
-  maxCounts: { red: number; green: number; blue: number }
-): number {
+function powerOfSet(set: { red: number; green: number; blue: number }): number {
+  return set.red * set.green * set.blue
+}
+
+function sumPowersOfMinSets(games: string[]): number {
   return games
     .map(parseGame)
-    .filter((game) => isGamePossible(game, maxCounts))
-    .reduce((sum, game) => sum + game.id, 0)
+    .map(minCubesForGame)
+    .map(powerOfSet)
+    .reduce((sum, power) => sum + power, 0)
 }
 
 async function main({ debug }: { debug: boolean } = { debug: false }) {
@@ -57,7 +84,7 @@ async function main({ debug }: { debug: boolean } = { debug: false }) {
   )
 
   const maxCounts = { red: 12, green: 13, blue: 14 }
-  const solution = sumPossibleGameIds(games, maxCounts)
+  const solution = sumPowersOfMinSets(games)
   console.log(`Sum of IDs of possible games: ${solution}`)
   return solution
 }
