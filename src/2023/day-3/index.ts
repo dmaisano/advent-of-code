1e9a93d7321440eda8f3a68bf89f30888dc2634b
Dom <dmaisanooo@gmail.com>
2023-12-31 01:39:02 -0500

    Day 3 Part 2

--- src/2023/day-3/index.ts
+++ src/2023/day-3/index.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { importFileStrArray } from "../utils"
 
 type Symbol = "*" | "#" | "+" | "$"
@@ -29,6 +30,69 @@ function part1Soln(lines: string[]) {
   return result
 }
 
+function part2Soln(lines: string[]) {
+  const resultArray: string[] = []
+
+  const numberIndices = []
+  for (let i = 0; i < lines.length; i++) {
+    const numbers = lines[i].replace(/\./g, " ")
+
+    for (const match of numbers.matchAll(/\*/g)) {
+      for (let j = match.index; j < match.index + match[0].length; j++) {
+        const surrounding = [
+          (lines[i - 1] ?? "")[j - 1] ?? ".",
+          (lines[i - 1] ?? "")[j] ?? ".",
+          (lines[i - 1] ?? "")[j + 1] ?? ".",
+          (lines[i] ?? "")[j - 1] ?? ".",
+          (lines[i] ?? "")[j] ?? ".",
+          (lines[i] ?? "")[j + 1] ?? ".",
+          (lines[i + 1] ?? "")[j - 1] ?? ".",
+          (lines[i + 1] ?? "")[j] ?? ".",
+          (lines[i + 1] ?? "")[j + 1] ?? ".",
+        ]
+        const indices = [
+          [i - 1, j - 1],
+          [i - 1, j],
+          [i - 1, j + 1],
+          [i, j - 1],
+          [i, j],
+          [i, j + 1],
+          [i + 1, j - 1],
+          [i + 1, j],
+          [i + 1, j + 1],
+        ]
+        const localNumberIndices = []
+        for (let k = 0; k < surrounding.length; k++) {
+          if (
+            /\d/.test(surrounding[k]) &&
+            (!/\d/.test(surrounding[k - 1] ?? "") || k % 3 == 0)
+          )
+            localNumberIndices.push(indices[k])
+        }
+        if (localNumberIndices.length == 2)
+          numberIndices.push(...localNumberIndices)
+      }
+    }
+  }
+
+  for (const index of numberIndices) {
+    const [i, j] = index
+    const line = lines[i]
+    const num = ["", "", "", line[j], "", "", ""]
+    if (/\d/.test(line[j - 1] ?? "")) num[2] = line[j - 1]
+    if (num[2] != "" && /\d/.test(line[j - 2] ?? "")) num[1] = line[j - 2]
+    if (num[1] != "" && /\d/.test(line[j - 3] ?? "")) num[0] = line[j - 3]
+    if (/\d/.test(line[j + 1] ?? "")) num[4] = line[j + 1]
+    if (num[4] != "" && /\d/.test(line[j + 2] ?? "")) num[5] = line[j + 2]
+    if (num[5] != "" && /\d/.test(line[j + 3] ?? "")) num[6] = line[j + 3]
+    resultArray.push(num.join(""))
+  }
+  return resultArray.reduce(
+    (a, x, i, r) => a + (i % 2 == 0 ? parseInt(x) * parseInt(r[i + 1]) : 0),
+    0
+  )
+}
+
 async function main({ debug }: { debug: boolean } = { debug: false }) {
   const lines = await importFileStrArray(
     "./input.txt",
@@ -45,9 +109,8 @@ async function main({ debug }: { debug: boolean } = { debug: false }) {
       ".664.598..",
     ]
   )
-  // const schematic = lines.map((row) => row.split(""))
 
-  const sumOfNumbers = part1Soln(lines)
+  const sumOfNumbers = part2Soln(lines)
   console.log(
     `Sum of all of the part numbers in the engine schematic: ${sumOfNumbers}`
   )
--- src/2023/utils/index.ts
+++ src/2023/utils/index.ts
@@ -1,13 +1,14 @@
 import { readFile } from "fs/promises"
-import { join as joinPath } from "path"
+import path from "path"
 
 export const importFileStrArray = async (
-  fileName: string,
+  filePath: string,
   stub: string[] = []
 ): Promise<string[]> => {
   if (stub?.length > 0) return stub
 
-  return (await readFile(joinPath(process.cwd(), fileName), "utf8"))
+  filePath = path.join(path.resolve(process.argv[1], ".."), filePath)
+  return (await readFile(filePath, "utf8"))
     .split("\n")
     .map((l) => l.trim())
     .filter((l) => l.length > 0)
