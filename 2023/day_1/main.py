8deee2fbfc3c51185b21b077e0b0441d8bd6429a
Dom <dmaisanooo@gmail.com>
2024-01-05 23:33:40 -0500

    Day 1 Part 2

--- 2023/day_1/main.py
+++ 2023/day_1/main.py
@@ -1,9 +1,22 @@
-import sys
 import os
+import re
+import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import utils
+
+digit_mapping = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
 
 def sum_calibration_values_part_1(calibration_document: list[str]) -> int:
@@ -16,14 +29,49 @@ def sum_calibration_values_part_1(calibration_document: list[str]) -> int:
     return total_sum
 
 
+def sum_calibration_values_part_2(calibration_document: list[str]) -> int:
+    total_sum = 0
+    for line in calibration_document:
+        total_sum += parse_line(line)
+    return total_sum
+
+
+def parse_line(line: str) -> int:
+    digits: list[str] = []
+
+    for i in range(0, len(line)):
+        char = line[i]
+        if char.isdigit():
+            digits.append((char))
+            continue
+
+        sub_str = line[i:]
+        for [key, value] in digit_mapping.items():
+            if sub_str.startswith(key):
+                digits.append(value)
+                break
+
+    return int(digits[0] + digits[-1])
+
+
 if __name__ == "__main__":
     debug = False
-    stub: list[str] = debug and [
+    stub_part_2: list[str] = debug and [
+        "two1nine",
+        "eightwothree",
+        "abcone2threexyz",
+        "xtwone3four",
+        "4nineeightseven2",
+        "zoneight234",
+        "7pqrstsixteen",
+    ]
+    stub_part_1: list[str] = debug and [
         "1abc2",
         "pqr3stu8vwx",
         "a1b2c3d4e5f",
         "treb7uchet",
     ]
-    calibration_document = utils.read_file_lines("input.txt", stub)
-    points = sum_calibration_values_part_1(calibration_document)
+
+    calibration_document = utils.read_file_lines("input.txt", stub_part_2)
+    points = sum_calibration_values_part_2(calibration_document)
     print(f"Total points: {points}")
